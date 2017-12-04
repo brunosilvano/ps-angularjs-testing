18d578aeac135f27eaf6cd046013a45c0ed6cf02
Bruno Silvano Costa <bruno_705@hotmail.com>
2017-12-03 23:34:09 -0200

    Code refactoring

--- spec/expenses-data.service.spec.js
+++ spec/expenses-data.service.spec.js
@@ -3,6 +3,7 @@ describe('expensesDataService', () => {
   beforeEach(module('app'));
 
   it('should return three expenses items', inject(function (expensesDataService) {
-     expect(expensesDataService.getExpenses().length).toBe(3);
+     expect(expensesDataService.getExpenses().length).toBe(3); 
   }));
+
 });
